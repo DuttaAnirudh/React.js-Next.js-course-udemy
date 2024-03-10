cbb84cf39e8e37c5d7ec7e085660594d8e31ed48
DuttaAnirudh <worktodutta@gmail.com>
2024-03-10 21:11:43 +0530

    build eat-n-split static app

---
+++ 05-eat-n-split/eat-n-split/src/components/Friend.js
@@ -0,0 +1,22 @@
+import Button from "./Button.js";
+
+export default function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      <p
+        className={
+          friend.balance === 0 ? "" : friend.balance > 0 ? "green" : "red"
+        }
+      >
+        {friend.balance === 0
+          ? `you and ${friend.name} are even`
+          : friend.balance > 0
+          ? `${friend.name} owes you $${Math.abs(friend.balance)}`
+          : `you owe ${friend.name} $${Math.abs(friend.balance)}`}
+      </p>
+      <Button>Select</Button>
+    </li>
+  );
+}
