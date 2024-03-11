fcb6e381313a8c53e549fe57a8d46fe77b09f8d3
DuttaAnirudh <worktodutta@gmail.com>
2024-03-11 18:36:31 +0530

    add functionality to split bill form of eat-n-split app

--- 05-eat-n-split/eat-n-split/src/components/FormSplitBill.js
+++ 05-eat-n-split/eat-n-split/src/components/FormSplitBill.js
@@ -1,23 +1,56 @@
+import { useState } from "react";
 import Button from "./Button.js";
 
-export default function FormSplitBill({ selectedFriend }) {
+export default function FormSplitBill({ selectedFriend, onSplitBill }) {
+  const [billValue, setBillValue] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = billValue ? billValue - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!billValue || !paidByUser) {
+      return;
+    }
+    onSplitBill(whoIsPaying === "user" ? paidByFriend : -paidByUser);
+
+    setBillValue("");
+    setPaidByUser("");
+    setWhoIsPaying("user");
+  }
+
   return (
-    <form className="form-split-bill">
+    <form className="form-split-bill" onSubmit={handleSubmit}>
       <h2>Split the bill with {selectedFriend.name}</h2>
 
       <label>💳 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={billValue}
+        onChange={(e) => setBillValue(+e.target.value)}
+      />
 
       <label>🙍‍♂️ Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            +e.target.value > billValue ? paidByUser : +e.target.value
+          )
+        }
+      />
 
       <label>🧔 {selectedFriend.name}'s expense</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
 
       <label>🤑 Who is paying the bill</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
-        <option value={selectedFriend.id}>{selectedFriend.name}</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
 
       <Button>Split Bill</Button>
