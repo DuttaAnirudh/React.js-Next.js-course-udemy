2b962d8b67d9a4c301a253670a628cd22e4e7f10
DuttaAnirudh <worktodutta@gmail.com>
2024-04-15 16:38:08 +0530

    feat: add a fake authentication login

--- 10-worldwise/worldwise/src/App.jsx
+++ 10-worldwise/worldwise/src/App.jsx
@@ -10,23 +10,26 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="product" element={<Product />} />
-      <Route path="pricing" element={<Pricing />} />
-      <Route path="login" element={<Login />} />
-      <Route path="app" element={<AppLayout />}>
-        <Route index element={<Navigate replace to="cities" />} />
-        <Route path="cities" element={<CityList />} />
-        <Route path="cities/:id" element={<City />} />
-        <Route path="countries" element={<CountryList />} />
-        <Route path="form" element={<Form />} />
-      </Route>
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<Navigate replace to="cities" />} />
+          <Route path="cities" element={<CityList />} />
+          <Route path="cities/:id" element={<City />} />
+          <Route path="countries" element={<CountryList />} />
+          <Route path="form" element={<Form />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
