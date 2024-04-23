cd6df172fd384e443aee3f05f957a5633c2f46c1
DuttaAnirudh <worktodutta@gmail.com>
2024-04-23 21:07:38 +0530

    feat: set up tailwind css  environment and add styling to homepage and new order page using tailwind classes

--- 14-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 14-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,31 +1,31 @@
-import { useState } from "react";
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant";
+import { useState } from 'react';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -34,7 +34,7 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting = navigation.state === 'submitting';
 
   // Fetching an object of errors with respective error messages
   const formErrors = useActionData();
@@ -48,13 +48,13 @@ function CreateOrder() {
       <Form method="post">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer" required className="input" />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required className="input" />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -62,18 +62,33 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input type="text" name="address" required className="input" />
           </div>
         </div>
 
         <div>
-          <input type="checkbox" name="priority" id="priority" />
+          <input
+            type="checkbox"
+            name="priority"
+            id="priority"
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring 
+            focus:ring-yellow-400 focus:ring-offset-2"
+          />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>Order now</button>
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-400 px-4 
+            py-3 font-semibold uppercase tracking-wide text-stone-800
+            transition-colors duration-300 hover:bg-yellow-300 
+            focus:outline-none focus:ring focus:ring-yellow-300 
+            focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
+            Order now
+          </button>
         </div>
       </Form>
     </div>
@@ -87,7 +102,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === 'on',
   };
 
   // An object for errors
@@ -96,7 +111,7 @@ export async function action({ request }) {
   // Validating Phone Number & Mutating errors object if the phone number is NOT valid
   if (!isValidPhone(order.phone)) {
     errors.phone =
-      "Please give us your correct phone number. We might need it to contact you.";
+      'Please give us your correct phone number. We might need it to contact you.';
   }
 
   // returning errors object if there are errors and NOT redrecting to differnt page
---
+++ 14-fast-react-pizza/src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className=" hidden text-sm font-semibold md:block ">Anirudh</div>;
+}
+
+export default Username;
