ab856f2ff303cc53f2dfb21a8c9ddc5a7d8e08e3
DuttaAnirudh <worktodutta@gmail.com>
2024-04-24 19:36:05 +0530

    feat: add styling to menu page, cart, order form and order overview page

--- 14-fast-react-pizza/src/features/menu/Menu.jsx
+++ 14-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,11 +1,11 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
   const menuData = useLoaderData();
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menuData.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
--- 14-fast-react-pizza/src/ui/Header.jsx
+++ 14-fast-react-pizza/src/ui/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
       className="flex items-center justify-between border-b border-stone-200 
     bg-yellow-500 px-4 py-3 uppercase sm:px-6"
     >
-      <Link to="/" className=" tracking-widest">
+      <Link to="/" className="  tracking-widest">
         Fast React Pizza Co.
       </Link>
 
